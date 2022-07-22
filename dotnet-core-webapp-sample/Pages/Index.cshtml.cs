f8e0ff345a88f79b20cb1b755c64245047823a55
Filipe Farinha <ffarinha@microsoft.com>
2022-07-22 16:58:21 +0100

    second commit

--- dotnet-core-webapp-sample/Pages/Index.cshtml.cs
+++ dotnet-core-webapp-sample/Pages/Index.cshtml.cs
@@ -1,25 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace dotnet_core_webapp_sample.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
+            var SA_ConnectionString = _configuration.GetConnectionString("BlobConnection");
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(SA_ConnectionString);
 
+            //Create a unique name for the container
+            string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+            // Create the container and return a container client object
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
         }
     }
 }
